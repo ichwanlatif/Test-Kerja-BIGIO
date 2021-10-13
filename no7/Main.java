4e20c4dcc1c6620701042941cafe82da85b1b32d
ichwanlatif <55022772+ichwanlatif@users.noreply.github.com>
2021-10-13 19:57:34 +0700

    delete comment no7

--- no7/Main.java
+++ no7/Main.java
@@ -6,7 +6,6 @@ import java.math.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         Scanner kb = new Scanner(System.in);
         int test_cases = kb.nextInt();
         for(int cs = 1; cs <= test_cases; cs++){
